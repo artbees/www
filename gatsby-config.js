265e2961acb91494afb4aa831cf983b2b4057b4f
Inan Onder Guden <30871191+guden@users.noreply.github.com>
2020-10-15 17:48:59 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Artbees`,
+    author: `Artbees`,
+    description: `Artbees on Gatsby...`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/artbees`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/artbees`,
       },
     ],
   },
